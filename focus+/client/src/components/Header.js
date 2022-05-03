4cc4839c42948f42d6808ceba8027b40880dd4d2
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-05-03 14:06:23 +0200

    [ADD] header

---
+++ focus+/client/src/components/Header.js
@@ -0,0 +1,32 @@
+//////////////////
+/* Dependencies */
+//////////////////
+
+////////////
+/* STYLES */
+////////////
+
+import "../styles/Header.css";
+
+//////////
+/* INIT */
+//////////
+
+import LogoMinistère from "../image/logo_ministère.png"
+//import LogoFocus from "../image/logo_focus+.png"
+
+/////////
+/* APP */
+/////////
+
+function Header() {
+    return (
+        <header>
+            <div className="logo-ministère">
+                <img src={LogoMinistère} alt="logo du ministère de l'Education Nationale" title="logo du ministère de l'Education Nationale"></img>
+            </div>
+        </header>
+    );
+}
+
+export default Header
