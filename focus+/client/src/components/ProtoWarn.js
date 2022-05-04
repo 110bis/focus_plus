de4c06d9bbe151ad6626e9b50c5ab1bf9dd4ec7d
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-05-04 12:20:40 +0200

    [ADD] message warning proto

---
+++ focus+/client/src/components/ProtoWarn.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/ProtoWarn.css"
+
+function ProtoWarn() {
+    return (
+        <div className="Proto">
+            <p className="Msg">Ceci est un Prototype de Focus+</p>
+        </div>
+    )
+}
+
+export default ProtoWarn;
