16c766a105f01b559d6df5ad2376b6cba63c0c54
Quentin TREHEUX <quentin.treheux@epitech.eu>
2022-05-04 12:22:25 +0200

    [UPD] add about pages

--- focus+/client/src/index.js
+++ focus+/client/src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 // Import pages //
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Error from "./pages/Error";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,6 +14,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" exact element={<Home />} />
+      <Route path="/about" exact element={<About />} />
       <Route path="*" element={<Error />} options={{ status: 404}} />
     </Routes>
   </BrowserRouter>
